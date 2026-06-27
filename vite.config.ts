6bf9f777063ed28762ddaa4f8e4c8c4f73c9c12a
Nimish Suri <nimishsuri01@gmail.com>
2026-06-27 09:11:24 +0530

    Fix: Update base path to / for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Bus-Scheduling-System/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
